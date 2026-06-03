34c01d77fe8fb79ce5dc1a99b24578a1595a7e2a
Kibic34 <tom.srodon@gmail.com>
2026-06-03 20:59:21 +0200

    script.py

--- script.py
+++ script.py
@@ -1,5 +1,4 @@
 from playwright.sync_api import sync_playwright
-import time
 
 with sync_playwright() as p:
     browser = p.chromium.launch()
@@ -7,10 +6,20 @@ with sync_playwright() as p:
 
     page.goto("https://hydro.imgw.pl/#/list/hydro?rpp=20&pf=0&c=229&cols=c,n,r,ic,csv,csd,tc,wv,dtw,dta,mdf,av")
 
-    # czekaj aż dane się załadują
+    # poczekaj aż dane się załadują
     page.wait_for_timeout(15000)
 
-    # kliknij ikonę Zapisz (ważne!)
-    page.click("button[title='Zapisz']")
+    # ✅ kliknij ikonę "drukuj"
+    page.click("button[title='Drukuj']")
+
+    # ✅ poczekaj aż widok się zmieni (bardzo ważne)
+    page.wait_for_timeout(5000)
+
+    # ✅ TERAZ generujemy PDF (bez okna systemowego)
+    page.pdf(
+        path="raport.pdf",
+        format="A4",
+        print_background=True
+    )
 
     browser.close()
